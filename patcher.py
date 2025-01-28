f46b78a7bc5ec50418f5de7c9bf38d96d9450f36
Nathanaël Jourdane <njourdane@protonmail.com>
2025-01-28 13:45:54 +0100

    scale poc

--- patcher.py
+++ patcher.py
@@ -1,11 +1,20 @@
 import fontforge
+import psMat
 
+print("Importing font...")
 font_source = fontforge.open("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf")
 font_source.selection.select(("ranges", None), "A", "Z")
 font_source.copy()
 
+print("Generating new font...")
 font_new = fontforge.font()
 font_new.selection.select(("ranges", None), "A", "Z")
 font_new.paste()
+# font_new.addExtrema()
+
+font_new.selection.select(("ranges", None), "A", "D")
+font_new.transform(psMat.scale(0.5))
+
 font_new.fontname = "NewFont"
+
 font_new.generate("./NewFont.ttf")
