1001c94804444a70280dbe459b919aa0781cac7b
Nathanaël Jourdane <njourdane@protonmail.com>
2025-01-29 19:49:22 +0100

    generalyse glyph merge

--- glyph_merge.py
+++ glyph_merge.py
@@ -5,21 +5,34 @@ import psMat
 INPUT_FONT_PATH = Path("/usr/share/fonts/opentype/artemisia/GFSArtemisia.otf")
 OUTPUT_FONT_PATH = Path.home() / ".fonts" / "GFSArtemisia.otf"
 
+GLYPH_SCALE = 0.8
+VERTICAL_TRANSLATE_RATIO = 0.35
+VERTICAL_OFFSET_RATIO = 0.15
 
 font = fontforge.open(str(INPUT_FONT_PATH))
+vmove_ratio = font.em * VERTICAL_TRANSLATE_RATIO
+voffset_ratio = font.em * VERTICAL_OFFSET_RATIO
 
-layer_a = font["a"].layers[font["a"].activeLayer]
-layer_b = font["b"].layers[font["b"].activeLayer]
-layer_c = font["c"].layers[font["c"].activeLayer]
-layer_d = font["d"].layers[font["d"].activeLayer]
 
-layer_a.transform(psMat.translate(0, 400))
-layer_b.transform(psMat.translate(0, -400))
-layer_c.transform(psMat.translate(font["a"].width, 400))
-layer_d.transform(psMat.translate(font["b"].width, -400))
+def build_layer(chars: list[str], y_offset):
+    x_offset = 0
+    layer = fontforge.layer()
+    for char in chars:
+        char_layer = font[char].layers[font[char].activeLayer]
+        char_layer.transform(psMat.translate(x_offset, y_offset))
+        layer += char_layer
+        x_offset += font[char].width
+    return layer, x_offset
 
-font["c"].layers[font["c"].activeLayer] = layer_a + layer_b + layer_c + layer_d
-font["c"].width = max(font["a"].width + font["c"].width, font["b"].width + font["d"].width)
-font["c"].transform(psMat.scale(0.8))
 
+def build_multi_glyph(name: str, top_chars: list[str], bottom_chars: list[str]):
+    layer_top, top_width = build_layer(top_chars, voffset_ratio + vmove_ratio)
+    layer_btm, btm_width = build_layer(bottom_chars, voffset_ratio - vmove_ratio)
+
+    font[name].layers[font[name].activeLayer] = layer_top + layer_btm
+    font[name].width = max(top_width, btm_width)
+    font[name].transform(psMat.scale(GLYPH_SCALE))
+
+
+build_multi_glyph("u", ["a", "c"], ["e", "m"])
 font.generate(str(OUTPUT_FONT_PATH))
