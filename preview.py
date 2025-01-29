da9824981c164c0665c82ad4340b7de6c0b96f07
Nathanaël Jourdane <njourdane@protonmail.com>
2025-01-29 17:36:13 +0100

    improve preview script

--- preview.py
+++ preview.py
@@ -1,13 +1,23 @@
 """
 Utility script that:
-1. Build a svg file containing a text;
-2. Copy font file to user font dir;
-3. Convert the svg to png with Inkscape.
-Note: cairosvg does not support ligatures but Inkscape does, hence the system call.
+1. Build a svg file containing a text using the font;
+2. Convert the svg to png with Inkscape.
+Note: cairosvg does not support ligatures but Inkscape does,
+hence the system call.
 """
 
 from pathlib import Path
 import subprocess
+from sys import argv, exit
+
+
+if len(argv) != 3:
+    print(f'usage: { argv[0] } <font name> <preview text>')
+    print(f'example: { argv[0] } "GFS Artemisia" "Hello|world"')
+    exit(1)
+
+font_name = argv[1]
+text = argv[2]
 
 
 FONT_DIR = Path.home() / '.fonts'
@@ -15,19 +25,17 @@ OUTPUT_DIR = Path('./output')
 
 SVG_PATH = OUTPUT_DIR / 'preview.svg'
 PNG_PATH = OUTPUT_DIR / 'preview.png'
-FONT_PATH = OUTPUT_DIR / 'Aligaturetestfont-Regular.ttf'
-FONT_NAME = "A ligature test font"
-TEXT = "noeud"
-WIDTH, HEIGHT = 300, 200
 
+WIDTH, HEIGHT = 300, 200
+FONT_SIZE = 40
 
 CSS = f'''
 text {{
-    font-family: "{ FONT_NAME }";
+    font-family: "{ font_name }";
     text-anchor: middle;
     alignment-baseline: middle;
     fill: darkslategrey;
-    font-size: 60;
+    font-size: { FONT_SIZE };
 }}
 '''
 
@@ -40,13 +48,16 @@ SVG_SUFFIX = '''
 </svg>
 '''
 
-CMD = f"inkscape { SVG_PATH } --export-filename={ PNG_PATH }"
+CMD_RASTERIZE = f"inkscape { SVG_PATH } --export-filename={ PNG_PATH }"
 
-svg = f'<text x="{ WIDTH/2 }px" y="{ HEIGHT/2 }px">{ TEXT }</text>'
+svg = ''
+subtexts = text.split("|")
+for idx, subtext in enumerate(subtexts):
+    height = idx * FONT_SIZE * 1.1 + HEIGHT / (len(subtexts) + 0.5)
+    svg += f'<text x="{ WIDTH/2 }px" y="{ height }px">{ subtext }</text>\n'
 
 SVG_PATH.parent.mkdir(parents=True, exist_ok=True)
 with open(SVG_PATH, 'w', encoding='utf-8') as svg_file:
     svg_file.write(f'{ SVG_PREFIX }{ svg }{ SVG_SUFFIX }')
 
-(FONT_DIR / FONT_PATH.name).write_bytes(FONT_PATH.read_bytes())
-subprocess.call(CMD, shell=True)
+subprocess.call(CMD_RASTERIZE, shell=True)
